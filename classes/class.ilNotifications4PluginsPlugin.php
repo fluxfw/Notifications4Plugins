0a7f4ba1eac402d7b68c3f03b5016b7e919e5036
Florian <fw@studer-raimann.ch>
2018-03-12 15:02:30 +0100

    Plugin uninstaller

--- classes/class.ilNotifications4PluginsPlugin.php
+++ classes/class.ilNotifications4PluginsPlugin.php
@@ -2,53 +2,71 @@
 
 require_once("./Services/UIComponent/classes/class.ilUserInterfaceHookPlugin.php");
 
-if (is_file('./Services/ActiveRecord/class.ActiveRecord.php')) {
-    require_once('./Services/ActiveRecord/class.ActiveRecord.php');
-} elseif (is_file('./Customizing/global/plugins/Libraries/ActiveRecord/class.ActiveRecord.php')) {
-    require_once('./Customizing/global/plugins/Libraries/ActiveRecord/class.ActiveRecord.php');
-}
+require_once('./Services/ActiveRecord/class.ActiveRecord.php');
 
 /**
  * Class ilNotifications4PluginsPlugin
  *
  * @author Stefan Wanzenried <sw@studer-raimann.ch>
  */
-class ilNotifications4PluginsPlugin extends ilUserInterfaceHookPlugin
-{
+class ilNotifications4PluginsPlugin extends ilUserInterfaceHookPlugin {
 
-    /**
-     * @var ilNotifications4PluginsPlugin
-     */
-    protected static $instance;
-
-
-    /**
-     * Singleton Access to this plugin
-     *
-     * @return ilNotifications4PluginsPlugin
-     */
-    public static function getInstance()
-    {
-        if (is_null(self::$instance)) {
-            self::$instance = new self();
-        }
-
-        return self::$instance;
-    }
+	/**
+	 * @var ilNotifications4PluginsPlugin
+	 */
+	protected static $instance;
 
 
-    /**
-     * Get Plugin Name. Must be same as in class name il<Name>Plugin
-     * and must correspond to plugins subdirectory name.
-     *
-     * Must be overwritten in plugin class of plugin
-     * (and should be made final)
-     *
-     * @return    string    Plugin Name
-     */
-    public function getPluginName()
-    {
-        return 'Notifications4Plugins';
-    }
+	/**
+	 * Singleton Access to this plugin
+	 *
+	 * @return ilNotifications4PluginsPlugin
+	 */
+	public static function getInstance() {
+		if (is_null(self::$instance)) {
+			self::$instance = new self();
+		}
 
-} 
+		return self::$instance;
+	}
+
+
+	/**
+	 * @var ilDB
+	 */
+	protected $db;
+
+
+	public function __construct() {
+		parent::__construct();
+
+		global $DIC;
+
+		$this->db = $DIC->database();
+	}
+
+
+	/**
+	 * Get Plugin Name. Must be same as in class name il<Name>Plugin
+	 * and must correspond to plugins subdirectory name.
+	 *
+	 * Must be overwritten in plugin class of plugin
+	 * (and should be made final)
+	 *
+	 * @return    string    Plugin Name
+	 */
+	public function getPluginName() {
+		return 'Notifications4Plugins';
+	}
+
+
+	protected function beforeUninstall() {
+		require_once './Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/Notifications4Plugins/classes/Notification/class.srNotification.php';
+		require_once './Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/Notifications4Plugins/classes/Notification/class.srNotificationLanguage.php';
+
+		$this->db->dropTable(srNotification::TABLE_NAME, false);
+		$this->db->dropTable(srNotificationLanguage::TABLE_NAME, false);
+
+		return true;
+	}
+}
