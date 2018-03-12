258e0f6da48096354adb7e4bd58738db7352987b
Florian <fw@studer-raimann.ch>
2018-03-12 11:40:32 +0100

    Update languages

--- classes/Config/class.srNotificationConfigFormGUI.php
+++ classes/Config/class.srNotificationConfigFormGUI.php
@@ -7,128 +7,118 @@ require_once('./Services/Form/classes/class.ilMultiSelectInputGUI.php');
  *
  * @author Stefan Wanzenried <sw@studer-raimann.ch>
  */
-class srNotificationConfigFormGUI extends ilPropertyFormGUI
-{
+class srNotificationConfigFormGUI extends ilPropertyFormGUI {
 
-    /**
-     * @var
-     */
-    protected $parent_gui;
-
-    /**
-     * @var  ilCtrl
-     */
-    protected $ctrl;
-
-    /**
-     * @var ilNotifications4PluginsPlugin
-     */
-    protected $pl;
-
-    /**
-     * @var ilLanguage
-     */
-    protected $lng;
-
-    /**
-     * @var srNotification
-     */
-    protected $notification;
+	/**
+	 * @var
+	 */
+	protected $parent_gui;
+	/**
+	 * @var  ilCtrl
+	 */
+	protected $ctrl;
+	/**
+	 * @var ilNotifications4PluginsPlugin
+	 */
+	protected $pl;
+	/**
+	 * @var ilLanguage
+	 */
+	protected $lng;
+	/**
+	 * @var srNotification
+	 */
+	protected $notification;
 
 
-    /**
-     * @param $parent_gui
-     * @param srNotification $notification
-     */
-    public function __construct($parent_gui, srNotification $notification)
-    {
-        global $ilCtrl, $lng;
+	/**
+	 * @param                $parent_gui
+	 * @param srNotification $notification
+	 */
+	public function __construct($parent_gui, srNotification $notification) {
+		global $ilCtrl, $lng;
 
-        $this->parent_gui = $parent_gui;
-        $this->ctrl = $ilCtrl;
-        $this->lng = $lng;
-        $this->pl = ilNotifications4PluginsPlugin::getInstance();
-        $this->notification = $notification;
-        $this->setFormAction($this->ctrl->getFormAction($this->parent_gui));
-        $this->initForm();
-    }
-
+		$this->parent_gui = $parent_gui;
+		$this->ctrl = $ilCtrl;
+		$this->lng = $lng;
+		$this->pl = ilNotifications4PluginsPlugin::getInstance();
+		$this->notification = $notification;
+		$this->setFormAction($this->ctrl->getFormAction($this->parent_gui));
+		$this->initForm();
+	}
 
 
-    protected function initForm()
-    {
-        $this->setTitle('General');
+	protected function initForm() {
+		$this->setTitle($this->pl->txt('general'));
 
-        if ($id = $this->notification->getId()) {
-            $item = new ilNonEditableValueGUI('ID');
-            $item->setValue($id);
-            $this->addItem($item);
-        }
+		if ($id = $this->notification->getId()) {
+			$item = new ilNonEditableValueGUI($this->pl->txt('id'));
+			$item->setValue($id);
+			$this->addItem($item);
+		}
 
-        $item = new ilTextInputGUI('Name', 'name');
-        $item->setRequired(true);
-        $item->setValue($this->notification->getName());
-        $item->setInfo('The name is an additional unique identifier of a notification. This allows to easy retrieve srNotification objects by name, not only by ID');
-        $this->addItem($item);
+		$item = new ilTextInputGUI($this->pl->txt('name'), 'name');
+		$item->setRequired(true);
+		$item->setValue($this->notification->getName());
+		$item->setInfo($this->pl->txt('name_info'));
+		$this->addItem($item);
 
-        $item = new ilTextInputGUI('Title', 'title');
-        $item->setRequired(true);
-        $item->setValue($this->notification->getTitle());
-        $this->addItem($item);
+		$item = new ilTextInputGUI($this->pl->txt('title'), 'title');
+		$item->setRequired(true);
+		$item->setValue($this->notification->getTitle());
+		$this->addItem($item);
 
-        $item = new ilTextAreaInputGUI('Description', 'description');
-        $item->setValue($this->notification->getDescription());
-        $this->addItem($item);
+		$item = new ilTextAreaInputGUI($this->pl->txt('description'), 'description');
+		$item->setValue($this->notification->getDescription());
+		$this->addItem($item);
 
-        $item = new ilTextInputGUI('Default Language', 'default_language');
-        $item->setInfo('This language is substituted if you try to load a non-existing language or no language is provided');
-        $item->setValue($this->notification->getDefaultLanguage());
-        $item->setRequired(true);
-        $this->addItem($item);
+		$item = new ilTextInputGUI($this->pl->txt('default_language'), 'default_language');
+		$item->setInfo($this->pl->txt('default_language_name'));
+		$item->setValue($this->notification->getDefaultLanguage());
+		$item->setRequired(true);
+		$this->addItem($item);
 
-        $item = new ilHiddenInputGUI('notification_id');
-        $item->setValue($this->notification->getId());
-        $this->addItem($item);
+		$item = new ilHiddenInputGUI('notification_id');
+		$item->setValue($this->notification->getId());
+		$this->addItem($item);
 
-        foreach ($this->notification->getLanguages() as $language) {
-            $this->addInputsForLanguage($language);
-        }
+		foreach ($this->notification->getLanguages() as $language) {
+			$this->addInputsForLanguage($language);
+		}
 
-        // For a new language
-        $this->addInputsForLanguage();
+		// For a new language
+		$this->addInputsForLanguage();
 
-        $this->addCommandButtons();
-    }
-
-
-    /**
-     * @param string $language
-     */
-    protected function addInputsForLanguage($language = '')
-    {
-        $section = new ilFormSectionHeaderGUI();
-        $section->setTitle($language ? strtoupper($language) : 'Add new Language');
-        $this->addItem($section);
-
-        if (!$language) {
-            $item = new ilTextInputGUI('Language', 'language');
-            $this->addItem($item);
-        }
-
-        $item = new ilTextInputGUI('Subject', 'subject_' . $language);
-        $item->setValue($language ? $this->notification->getSubject($language) : '');
-        $this->addItem($item);
-
-        $item = new ilTextAreaInputGUI('Text', 'text_' . $language);
-        $item->setValue($language ? $this->notification->getText($language) : '');
-        $this->addItem($item);
-    }
+		$this->addCommandButtons();
+	}
 
 
-    protected function addCommandButtons()
-    {
-        $method = $this->notification->getId() ? 'update' : 'create';
-        $this->addCommandButton($method, $this->lng->txt('save'));
-        $this->addCommandButton('cancel', $this->lng->txt('cancel'));
-    }
+	/**
+	 * @param string $language
+	 */
+	protected function addInputsForLanguage($language = '') {
+		$section = new ilFormSectionHeaderGUI();
+		$section->setTitle($language ? strtoupper($language) : $this->pl->txt('add_new_language'));
+		$this->addItem($section);
+
+		if (!$language) {
+			$item = new ilTextInputGUI($this->pl->txt('language'), 'language');
+			$this->addItem($item);
+		}
+
+		$item = new ilTextInputGUI($this->pl->txt('subject'), 'subject_' . $language);
+		$item->setValue($language ? $this->notification->getSubject($language) : '');
+		$this->addItem($item);
+
+		$item = new ilTextAreaInputGUI($this->pl->txt('text'), 'text_' . $language);
+		$item->setValue($language ? $this->notification->getText($language) : '');
+		$this->addItem($item);
+	}
+
+
+	protected function addCommandButtons() {
+		$method = $this->notification->getId() ? 'update' : 'create';
+		$this->addCommandButton($method, $this->pl->txt('save'));
+		$this->addCommandButton('cancel', $this->pl->txt('cancel'));
+	}
 }
--- classes/class.ilNotifications4PluginsConfigGUI.php
+++ classes/class.ilNotifications4PluginsConfigGUI.php
@@ -12,177 +12,164 @@ require_once('./Services/Utilities/classes/class.ilConfirmationGUI.php');
  *
  * @author Stefan Wanzenried <sw@studer-raimann.ch>
  */
-class ilNotifications4PluginsConfigGUI extends ilPluginConfigGUI
-{
+class ilNotifications4PluginsConfigGUI extends ilPluginConfigGUI {
 
-    /**
-     * @var ilNotifications4PluginsPlugin
-     */
-    protected $pl;
-
-    /**
-     * @var ilCtrl
-     */
-    protected $ctrl;
-
-    /**
-     * @var ilTemplate
-     */
-    protected $tpl;
-
-    /**
-     * @var ilToolbarGUI
-     */
-    protected $toolbar;
+	/**
+	 * @var ilNotifications4PluginsPlugin
+	 */
+	protected $pl;
+	/**
+	 * @var ilCtrl
+	 */
+	protected $ctrl;
+	/**
+	 * @var ilTemplate
+	 */
+	protected $tpl;
+	/**
+	 * @var ilToolbarGUI
+	 */
+	protected $toolbar;
 
 
-    public function __construct()
-    {
-        global $ilCtrl, $tpl, $ilToolbar;
+	public function __construct() {
+		global $ilCtrl, $tpl, $ilToolbar;
 
-        $this->pl = ilNotifications4PluginsPlugin::getInstance();
-        $this->ctrl = $ilCtrl;
-        $this->tpl = $tpl;
-        $this->toolbar = $ilToolbar;
-    }
-
-
-    /**
-     * @param $cmd
-     */
-    public function performCommand($cmd)
-    {
-        $cmd = $this->ctrl->getCmd('index');
-
-        switch ($cmd) {
-            case 'index':
-            case 'cancel':
-            case 'configure':
-                $this->index();
-                break;
-            case 'save':
-            case 'edit':
-            case 'create':
-            case 'update':
-            case 'delete':
-            case 'confirmDelete':
-            case 'add':
-                $this->$cmd();
-                break;
-        }
-    }
+		$this->pl = ilNotifications4PluginsPlugin::getInstance();
+		$this->ctrl = $ilCtrl;
+		$this->tpl = $tpl;
+		$this->toolbar = $ilToolbar;
+	}
 
 
-    /**
-     * Configure screen
-     */
-    public function index()
-    {
-        $button = ilLinkButton::getInstance();
-        $button->setUrl($this->ctrl->getLinkTarget($this, 'add'));
-        $button->setCaption($this->pl->txt('add_notification'), false);
-        $this->toolbar->addButtonInstance($button);
-        $table = new srNotificationTableGUI($this, 'index');
-        $this->tpl->setContent($table->getHTML());
-    }
+	/**
+	 * @param $cmd
+	 */
+	public function performCommand($cmd) {
+		$cmd = $this->ctrl->getCmd('index');
+
+		switch ($cmd) {
+			case 'index':
+			case 'cancel':
+			case 'configure':
+				$this->index();
+				break;
+			case 'save':
+			case 'edit':
+			case 'create':
+			case 'update':
+			case 'delete':
+			case 'confirmDelete':
+			case 'add':
+				$this->$cmd();
+				break;
+		}
+	}
 
 
-    public function add()
-    {
-        $form = new srNotificationConfigFormGUI($this, new srNotification());
-        $this->tpl->setContent($form->getHTML());
-    }
+	/**
+	 * Configure screen
+	 */
+	public function index() {
+		$button = ilLinkButton::getInstance();
+		$button->setUrl($this->ctrl->getLinkTarget($this, 'add'));
+		$button->setCaption($this->pl->txt('add_notification'), false);
+		$this->toolbar->addButtonInstance($button);
+		$table = new srNotificationTableGUI($this, 'index');
+		$this->tpl->setContent($table->getHTML());
+	}
 
 
-    public function edit()
-    {
-        $form = new srNotificationConfigFormGUI($this, srNotification::findOrFail((int) $_GET['notification_id']));
-        $this->tpl->setContent($form->getHTML());
-    }
+	public function add() {
+		$form = new srNotificationConfigFormGUI($this, new srNotification());
+		$this->tpl->setContent($form->getHTML());
+	}
 
 
-    public function create()
-    {
-        $form = new srNotificationConfigFormGUI($this, new srNotification());
-        if ($form->checkInput()) {
-            $service = new srNotificationService();
-            $service->create($form->getInput('title'), $form->getInput('description'), $form->getInput('name'), $form->getInput('default_language'), $this->getNotificationData($form));
-            ilUtil::sendSuccess('Created Notification', true);
-            $this->ctrl->redirect($this);
-        }
-
-        $form->setValuesByPost();
-        $this->tpl->setContent($form->getHTML());
-    }
+	public function edit() {
+		$form = new srNotificationConfigFormGUI($this, srNotification::findOrFail((int)$_GET['notification_id']));
+		$this->tpl->setContent($form->getHTML());
+	}
 
 
-    public function update()
-    {
-        /** @var srNotification $notification */
-        $notification = srNotification::findOrFail((int) $_POST['notification_id']);
-        $form = new srNotificationConfigFormGUI($this, $notification);
-        if ($form->checkInput()) {
-            $service = new srNotificationService($notification);
-            $service->update($form->getInput('title'), $form->getInput('description'), $form->getInput('name'), $form->getInput('default_language'), $this->getNotificationData($form, $notification));
-            ilUtil::sendSuccess('Updated Notification', true);
-            $this->ctrl->redirect($this);
-        }
+	public function create() {
+		$form = new srNotificationConfigFormGUI($this, new srNotification());
+		if ($form->checkInput()) {
+			$service = new srNotificationService();
+			$service->create($form->getInput('title'), $form->getInput('description'), $form->getInput('name'), $form->getInput('default_language'), $this->getNotificationData($form));
+			ilUtil::sendSuccess($this->pl->txt('created_notification'), true);
+			$this->ctrl->redirect($this);
+		}
 
-        $form->setValuesByPost();
-        $this->tpl->setContent($form->getHTML());
-    }
+		$form->setValuesByPost();
+		$this->tpl->setContent($form->getHTML());
+	}
 
 
-    public function confirmDelete()
-    {
-        $notification = srNotification::findOrFail((int) $_GET['notification_id']);
-        $gui = new ilConfirmationGUI();
-        $gui->setHeaderText('Are you sure you want to delete the Notification?');
-        $gui->setFormAction($this->ctrl->getFormAction($this));
-        $gui->setCancel('Cancel', 'cancel');
-        $gui->setConfirm('Delete', 'delete');
-        $gui->addItem('notification_id', $notification->getId(), $notification->getTitle());
-        $this->tpl->setContent($gui->getHTML());
-    }
+	public function update() {
+		/** @var srNotification $notification */
+		$notification = srNotification::findOrFail((int)$_POST['notification_id']);
+		$form = new srNotificationConfigFormGUI($this, $notification);
+		if ($form->checkInput()) {
+			$service = new srNotificationService($notification);
+			$service->update($form->getInput('title'), $form->getInput('description'), $form->getInput('name'), $form->getInput('default_language'), $this->getNotificationData($form, $notification));
+			ilUtil::sendSuccess($this->pl->txt('updated_notification'), true);
+			$this->ctrl->redirect($this);
+		}
+
+		$form->setValuesByPost();
+		$this->tpl->setContent($form->getHTML());
+	}
 
 
-    public function delete()
-    {
-        $notification = srNotification::findOrFail((int) $_POST['notification_id']);
-        $notification->delete();
-        ilUtil::sendSuccess('Deleted Notification', true);
-        $this->ctrl->redirect($this);
-    }
+	public function confirmDelete() {
+		$notification = srNotification::findOrFail((int)$_GET['notification_id']);
+		$gui = new ilConfirmationGUI();
+		$gui->setHeaderText($this->pl->txt('delete_confirm'));
+		$gui->setFormAction($this->ctrl->getFormAction($this));
+		$gui->setCancel($this->pl->txt('cancel'), 'cancel');
+		$gui->setConfirm($this->pl->txt('delete'), 'delete');
+		$gui->addItem('notification_id', $notification->getId(), $notification->getTitle());
+		$this->tpl->setContent($gui->getHTML());
+	}
 
-    /**
-     * @param srNotificationConfigFormGUI $form
-     * @param srNotification $notification
-     * @return array
-     */
-    protected function getNotificationData(srNotificationConfigFormGUI $form, srNotification $notification = null)
-    {
-        $data = array();
-        // New language added
-        if ($form->getInput('language')) {
-            $new = array();
-            $new['subject'] = $form->getInput('subject_');
-            $new['text'] = $form->getInput('text_');
-            $new['language'] = $form->getInput('language');
-            $data[] = $new;
-        }
 
-        // Update existing
-        if ($notification) {
-            foreach ($notification->getLanguages() as $language) {
-                $update = array();
-                $update['subject'] = $form->getInput('subject_' . $language);
-                $update['text'] = $form->getInput('text_' . $language);
-                $update['language'] = $language;
-                $data[] = $update;
-            }
-        }
+	public function delete() {
+		$notification = srNotification::findOrFail((int)$_POST['notification_id']);
+		$notification->delete();
+		ilUtil::sendSuccess($this->pl->txt('deleted_notification'), true);
+		$this->ctrl->redirect($this);
+	}
 
-        return $data;
-    }
 
+	/**
+	 * @param srNotificationConfigFormGUI $form
+	 * @param srNotification              $notification
+	 *
+	 * @return array
+	 */
+	protected function getNotificationData(srNotificationConfigFormGUI $form, srNotification $notification = NULL) {
+		$data = array();
+		// New language added
+		if ($form->getInput('language')) {
+			$new = array();
+			$new['subject'] = $form->getInput('subject_');
+			$new['text'] = $form->getInput('text_');
+			$new['language'] = $form->getInput('language');
+			$data[] = $new;
+		}
+
+		// Update existing
+		if ($notification) {
+			foreach ($notification->getLanguages() as $language) {
+				$update = array();
+				$update['subject'] = $form->getInput('subject_' . $language);
+				$update['text'] = $form->getInput('text_' . $language);
+				$update['language'] = $language;
+				$data[] = $update;
+			}
+		}
+
+		return $data;
+	}
 }
